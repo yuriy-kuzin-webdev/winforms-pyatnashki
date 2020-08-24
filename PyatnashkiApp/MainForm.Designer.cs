7eff85d5c45c35233423e51caeb7f60dce7bbb26
yuriykuzinpd <yuriy.kuzin.pd@gmail.com>
2020-08-24 21:06:10 +0300

    complete

---
+++ PyatnashkiApp/MainForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace PyatnashkiApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.markUp = new System.Windows.Forms.TableLayoutPanel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timesCounter = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.movesCounter = new System.Windows.Forms.ToolStripStatusLabel();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // markUp
+            // 
+            this.markUp.ColumnCount = 4;
+            this.markUp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.markUp.Location = new System.Drawing.Point(0, 0);
+            this.markUp.Name = "markUp";
+            this.markUp.RowCount = 4;
+            this.markUp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.markUp.Size = new System.Drawing.Size(800, 800);
+            this.markUp.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Silver;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.timesCounter,
+            this.toolStripStatusLabel3,
+            this.movesCounter});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 801);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(42, 17);
+            this.toolStripStatusLabel1.Text = "TIME : ";
+            // 
+            // timesCounter
+            // 
+            this.timesCounter.AutoSize = false;
+            this.timesCounter.Name = "timesCounter";
+            this.timesCounter.Size = new System.Drawing.Size(155, 17);
+            this.timesCounter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(51, 17);
+            this.toolStripStatusLabel3.Text = "Moves : ";
+            // 
+            // movesCounter
+            // 
+            this.movesCounter.Name = "movesCounter";
+            this.movesCounter.Size = new System.Drawing.Size(0, 17);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resetButton.Location = new System.Drawing.Point(600, 800);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(200, 23);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 823);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.markUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel markUp;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel timesCounter;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel movesCounter;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ PyatnashkiApp/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PyatnashkiApp
+{
+    public partial class MainForm : Form
+    {
+        //Точка отсчета времени
+        private DateTime startTime;
+        //Адресс пустой ячейки
+        private TableLayoutPanelCellPosition empty;
+        public MainForm()
+        {
+            InitializeComponent();
+
+            markUp.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            InitKeys();
+
+            resetButton.Click += (send, e) =>
+            {
+                timer1.Stop();
+                timesCounter.Text = "";
+                movesCounter.Text = "";
+                markUp.Controls.Clear();
+                InitKeys();
+            };
+
+            resetButton.PerformClick();
+
+            timer1.Tick += (send, e) =>
+                timesCounter.Text = new DateTime(DateTime.Now.Ticks - startTime.Ticks).ToString("mm:ss");
+        }
+
+        private void InitKeys()
+        {
+            Button[] buttons = new Button[16];
+            buttons.ToList().ForEach(btn =>
+            {
+                btn = new Button();
+                btn.Dock = DockStyle.Fill;
+                btn.BackColor = Color.Silver;
+                markUp.Controls.Add(btn);
+            });
+
+            Random r = new Random(DateTime.Now.Millisecond);
+            int text = 1;
+            List<Button> btns = markUp.Controls.Cast<Button>().ToList();
+            //Псевдосортировка перед назначением текста
+            btns.Sort((a, b) => r.Next(-1, 1));
+            btns.Sort((a, b) => r.Next(-1, 1));
+            btns.Sort((a, b) => r.Next(-1, 1));
+            btns.ForEach(btn => btn.Text = text++.ToString());
+            //Текст со значением 16 заменяем на пустой
+            btns.Last().Text = "";
+            
+            //Устанавливаем адресс пустой ячейки и подписуем на ивент
+            empty = markUp.GetPositionFromControl(btns.Last());
+            SubscribeNearestButtons();
+
+            //Второй вариант сортировки не работает при инициализации
+            //for (int i = 0; i < 139; i++)
+            //    //markUp.Controls.Cast<Button>().Where(btn => checkPosition(btn)).Take(new Random().Next(1, 4)).Last().PerformClick();
+            //    btns.Where(btn => checkPosition(btn))
+            //        .Take(new Random().Next(1, 4))
+            //        .Last().PerformClick();
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            //Получаем кнопки для замены значений
+            Button current = (Button)sender;
+            //Button next = markUp.Controls.Cast<Button>().First(btn => btn.Text == "");
+            Button next = markUp.GetControlFromPosition(empty.Column, empty.Row) as Button;
+
+            //Обмен значениями
+            next.Text = current.Text;
+            current.Text = "";
+
+            //Устанавливаем адресс пустой ячейки и подписуем на ивент
+            empty = markUp.GetPositionFromControl(current);
+            SubscribeNearestButtons();
+
+            //Обновляем счетчики
+            updateCounters();
+        }
+        
+        void SubscribeNearestButtons()
+        {
+            List<Button> buttons = markUp.Controls.Cast<Button>().ToList();
+            //Обновляем подписки 
+            buttons.ForEach(btn =>
+            {
+                if (checkPosition(btn))
+                    btn.Click += Btn_Click;
+                else
+                    btn.Click -= Btn_Click;
+            });
+        }
+        
+ 
+        bool checkPosition(Button button)
+        {
+            TableLayoutPanelCellPosition pos = markUp.GetPositionFromControl(button);
+            //Проверяем соседство по рядам
+            bool isRowNeighbor =
+                pos.Column == empty.Column && Math.Abs(empty.Row - pos.Row) == 1;
+            //По колонкам
+            bool isColumnNeighbor =
+                pos.Row == empty.Row && Math.Abs(empty.Column - pos.Column) == 1;
+
+            return isColumnNeighbor || isRowNeighbor;
+        }
+
+        
+        void updateCounters()
+        {
+            if (movesCounter.Text == "")
+            { 
+                movesCounter.Text = "1";
+                timerUpdate();
+            }
+            else
+            {
+                int count = Int32.Parse(movesCounter.Text);
+                movesCounter.Text = (++count).ToString();
+            }
+        }
+        void timerUpdate()
+        {
+            startTime = DateTime.Now;
+            timer1.Start();
+        }
+    }
+}
